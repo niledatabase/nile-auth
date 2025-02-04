961e427f4c5aa5447852adc31f9ae2bf05e5d65d
jrea <juzko.r@gmail.com>
2025-02-04 13:24:31 -0500

    fix: remove command parsing

--- packages/query/src/executeCommand.ts
+++ packages/query/src/executeCommand.ts
@@ -37,75 +37,36 @@ export async function executeCommand(params: {
   debug("pg client retrieved");
 
   try {
-    const commands = _command.text.split(";");
-    const cleanedCommands = commands
-      .map((cmd) => {
-        return cmd.trim();
-      })
-      .map((cmd) => {
-        return cmd.replace(/--.*/gim, "").trim();
-      })
-      .map((cmd) => {
-        // remove comments, db can't handle them - this may not be true, but too scared to remove
-        // https://stackoverflow.com/a/15123777
-        // https://www.techonthenet.com/postgresql/comments.php
-        const blockCommentsRemoved = cmd.replace(
-          /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm,
-          "",
-        );
-        return blockCommentsRemoved;
-      })
-      .filter(Boolean);
-    debug(`number of commands to be run: ${cleanedCommands.length}`);
-    let shouldStop = false;
-    const cmd = "";
-    // for (const cmd of cleanedCommands) {
-    if (shouldStop) {
-      info("Error occurred on previous command. No additional commands run.");
-      // break;
-    }
+    const commands = [_command.text];
+
     const start = Date.now();
-    const command = { ..._command, text: cmd };
+    const command = { ..._command, text: commands[0] };
     if (_command.rowMode !== "none") {
       command.rowMode = "array";
     }
 
-    debug(`Starting command ${_command.text} ${_command.values?.toString()}`);
+    const logCommand = command.text
+      ?.replace(/(\n|\r)/g, " ")
+      .replace(/\s+/g, " ")
+      .trim();
+    debug(`Starting command ${logCommand} ${_command.values?.toString()}`);
     const res = await client.query(_command).catch((e) => {
-      // attempt to sanitize syntax errors for actionable failures
-      // https://www.postgresql.org/docs/current/errcodes-appendix.html
-      const validErrorCodes = [
-        "08",
-        "2F000",
-        "3D000",
-        "3F000",
-        "53",
-        "54",
-        "55",
-        "57",
-        "XX",
-      ];
-      for (const code of validErrorCodes) {
-        if (e.code?.startsWith(code) || e.code === code) {
-          error("Failed command", e.message);
-        } else {
-          info("Failed command", e, command);
-          shouldStop = true;
-        }
-      }
-      const index = _command.text.indexOf(cmd);
+      error("Failed command", {
+        message: e.message,
+        text: logCommand,
+      });
+      const index = _command.text.indexOf(String(command.text));
       const beforeCommand = _command.text.substring(0, index);
       const lineNumber = beforeCommand.split("\n").length;
 
-      return { ...e, cmd, message: e.message, lineNumber };
+      return { ...e, message: e.message, lineNumber };
     });
     const stamp = Math.floor(Date.now() - start);
     debug(`Finished command in ${stamp}ms`);
     // }
+
     if (!Array.isArray(res)) {
-      // in the case this is an error, we're reasonably sure the last command is the one that caused the error.
-      // when queries are destructured, we need to map the error back, but the error only comes as an object, so fill and insert
-      return [...new Array(cleanedCommands.length - 1).fill(null), res];
+      return [res];
     }
     return res as unknown as QueryResult[];
   } catch (e: unknown) {
